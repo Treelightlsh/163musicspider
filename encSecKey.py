49eae1a4de5315df3ac9277bcc222fb3fc4d89ee
Treelight <43248389@qq.com>
2020-12-31 14:43:11 +0800

    222

--- encSecKey.py
+++ encSecKey.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 
 def random_charts(n):
@@ -16,15 +17,35 @@ def random_charts(n):
     return c
 
 
-def aes_parse(s):
-    """
-    对字串s进行解析
-    :param s:
-    :return:
-    """
-    l = []
-    for i in range(len(s)):
-        pass
+class AES_Parser(object):
+    def __init__(self, s):
+        self.sigBytes = 0
+        self.words = []
+        self.s = s
+        self.aes_parse()
+
+    def aes_parse(self):
+        """
+        对字串s进行解析
+        :param s:
+        :return:
+        """
+        # 计算结果列表长度
+        res_l_length = math.ceil(len(self.s) / 4)
+        # 定义结果列表
+        res_l = []
+        # 初始化结果列表的值均为0
+        for i in range(res_l_length):
+            res_l.append(0)
+        for i in range(len(self.s)):
+            res_l[i >> 2] |= (ord(self.s[i]) & 255) << 24 - 8 * (i % 4)
+        self.words = res_l
+        self.sigBytes = len(self.words) * 4
+
+
+
 
 if __name__ == '__main__':
-    print(random_charts(16))
+    aes_p1 = AES_Parser('0CoJUm6Qyw8W8jud')
+    aes_p2 = AES_Parser('0102030405060708')
+    print(aes_p2.sigBytes, aes_p2.words)
